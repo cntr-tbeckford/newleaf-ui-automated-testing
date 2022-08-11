314782cc6736d7c8dc9b71a7e3a7e6e16402a622
Tamoya Beckford <cntr_tbeckford@pennfoster,edu.com>
2022-08-11 08:28:43 -0500

    NLM-1736 DEC .feature using wrong URL's

--- cypress/integration/common/i_select_the_course.js
+++ cypress/integration/common/i_select_the_course.js
@@ -2,6 +2,7 @@ import { And } from "cypress-cucumber-preprocessor/steps";
 
 And('I select the default Program', ()=> {
     cy.log('selecting program');
+    // Selects the second option in the Program Listing that has the word Course
     cy.get('#course').select(1).should('include.text', 'Course');
     cy.log('selected program');
 })
